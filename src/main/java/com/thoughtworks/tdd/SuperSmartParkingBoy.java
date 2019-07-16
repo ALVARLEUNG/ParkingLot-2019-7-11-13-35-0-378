b49f35f8af57e3281981250912e2db49966bd705
ALVARLEUNG <alvar.leung@qq.com>
2019-07-16 08:30:23 +0800

    refactor: fixed NullPointerException in SuperSmartParkingBoy/parkCarInTheLot

--- src/main/java/com/thoughtworks/tdd/SuperSmartParkingBoy.java
+++ src/main/java/com/thoughtworks/tdd/SuperSmartParkingBoy.java
@@ -10,7 +10,7 @@ public class SuperSmartParkingBoy extends ParkingBoy{
     @Override
     public ParkingCarResult parkCarInTheLot(Car car) {
         if (null == car) return new ParkingCarResult();
-        ParkingLot parkingLot = this.getParkingLots().stream().reduce((item, items) -> (double)(1-item.getParkingCarTicket().size()/item.getLimit()) > (double) (1-items.getParkingCarTicket().size()/items.getLimit()) ? item : items).orElse(null);
+        ParkingLot parkingLot = this.getParkingLots().stream().reduce((item, items) -> (double)(1-item.getParkingCarTicket().size()/item.getLimit()) > (double) (1-items.getParkingCarTicket().size()/items.getLimit()) ? item : items).orElse(new ParkingLot());
         if (parkingLot.getParkingCarTicket().size() < 10) {
             return parkingLot.park(car);
         }
